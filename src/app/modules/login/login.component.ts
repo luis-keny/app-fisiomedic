b162fdfc7fc97067ede2e86beeb30027702b59de
Luis Keny Lucero Balvin <lucerobalvinluiskeny@gmail.com>
2024-07-27 16:19:11 -0500

    feat(img & dev): design and login validation logic

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  user: FormGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
 
+  constructor(
+    private router: Router
+  ) { }
+
+  onSubmit() {
+    const value = this.user.value;
+    if (value.name === 'fisiomedic.admin' && value.password === 'Admin123#') {
+      this.router.navigate(['home']);
+    }
+  }
 }
