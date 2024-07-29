5baddd146ec602d8fa707c9b44c5427f6fd809e5
Luis Keny Lucero Balvin <lucerobalvinluiskeny@gmail.com>
2024-07-29 17:12:46 -0500

    feat(design & add): design of the patient list view and addition of images for the icons

---
+++ src/app/modules/cliente-listado/card-cliente/card-cliente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-cliente',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-cliente.component.html',
+  styleUrl: './card-cliente.component.css'
+})
+export class CardClienteComponent {
+
+}
--- src/app/modules/cliente-listado/cliente-listado.component.ts
+++ src/app/modules/cliente-listado/cliente-listado.component.ts
@@ -1,20 +1,26 @@
 import { AfterViewInit, Component } from '@angular/core';
-import { RouterLink, RouterModule } from '@angular/router';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 import { BreadcrumbService } from '../../core/index.service.triggers';
+import { CardClienteComponent } from './card-cliente/card-cliente.component';
 
 @Component({
   selector: 'app-cliente-listado',
   standalone: true,
-  imports: [RouterLink, RouterModule],
+  imports: [RouterLink, CardClienteComponent],
   templateUrl: './cliente-listado.component.html',
   styleUrls: ['./cliente-listado.component.css', '/src/app/shared/css/header-views.css']
 })
 export class ClienteListadoComponent implements AfterViewInit {
   constructor(
-    private breadcrumbSrv: BreadcrumbService
+    private breadcrumbSrv: BreadcrumbService,
+    private router: Router
   ) { }
 
   ngAfterViewInit(): void {
     this.breadcrumbSrv.removeAllBread();
+  }
+
+  public goTo() {
+    this.router.navigate(['/home/cliente/hc']);
   }
 }
