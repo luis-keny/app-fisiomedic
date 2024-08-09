605c19b87c6510f8dbd05cb1a4cf81329a4c9ca0
Luis Keny Lucero Balvin <lucerobalvinluiskeny@gmail.com>
2024-08-08 20:03:42 -0500

    feat(localstorage): localstorage manager as models and functions

--- src/app/core/index.model.system.ts
+++ src/app/core/index.model.system.ts
@@ -1,3 +1,4 @@
 export * from './models/system/breadcrumb.model';
 export * from './models/system/notification.model';
 export * from './models/system/modal.model';
+export * from './models/system/localstorage.model';
